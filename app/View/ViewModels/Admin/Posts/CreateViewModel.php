020b2f5439ee167818dcbb746d136512598bda0a
John Edisson Ortiz <john.ortiz@placetopay.com>
2021-03-01 14:42:54 -0500

    refactor: Implemented ViewModel pattern in Admin Posts Controller

---
+++ app/View/ViewModels/Admin/Posts/CreateViewModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\ViewModels\Admin\Posts;
+
+use App\Models\Post;
+use App\View\ViewModels\FormViewModel;
+
+class CreateViewModel extends FormViewModel
+{
+    public function __construct()
+    {
+        parent::__construct(new Post());
+    }
+
+    public function action(): string
+    {
+        return route('admin.posts.store');
+    }
+
+    public function backRoute(): string
+    {
+        return route('admin.posts.index');
+    }
+
+    public function title(): string
+    {
+        return trans('posts.titles.create');
+    }
+
+    public function toArray(): array
+    {
+        return array_merge(parent::toArray(), [
+            'post' => $this->model(),
+        ]);
+    }
+}
