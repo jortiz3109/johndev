fb1369852ba120ac01fca3c090a2bb769652fe2c
John Edisson Ortiz <john.ortiz@placetopay.com>
2021-03-05 03:52:54 -0500

    fix: Fixed incorrect data on meta for twitter and og

--- resources/views/posts/show/meta/og.blade.php
+++ resources/views/posts/show/meta/og.blade.php
@@ -1,5 +1,5 @@
 <meta property="og:type" content="article" />
 <meta property="og:title" content="{{ $post->title }}" />
-<meta property="og:description" content="{{ $post->slug }}" />
+<meta property="og:description" content="{{ $post->summary }}" />
 <meta property="og:url" content="{{ route('posts.show', $post) }}" />
 <meta property="og:site_name" content="{{ config('app.name') }}" />
--- resources/views/posts/show/meta/twitter.blade.php
+++ resources/views/posts/show/meta/twitter.blade.php
@@ -1,4 +1,4 @@
 <meta name="twitter:title" content="{{ $post->title }}">
-<meta name="twitter:description" content="{{ $post->slug }}">
+<meta name="twitter:description" content="{{ $post->summary }}">
 <meta name="twitter:site" content="{{ config('site.social.twitter.account') }}">
 <meta name="twitter:creator" content="{{ config('site.social.twitter.account') }}">
